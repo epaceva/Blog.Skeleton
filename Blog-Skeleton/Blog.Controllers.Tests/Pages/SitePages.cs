57ae08c35213c9ac76e7983ab74a1b8d13646f0e
epaceva <paceva.eva@gmail.com>
2017-05-27 13:35:31 +0300

    Controllers.Tests are ready

---
+++ Blog-Skeleton/Blog.Controllers.Tests/Pages/SitePages.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers.Tests.Pages
+{
+    public partial class SitePages : BasePage
+    {
+        private String homeUrl = "http://localhost:60634/Article/List";
+        private String registrationUrl = "http://localhost:60634/Account/Register";
+        private String loginUrl = "http://localhost:60634/Account/Login";
+        private String helloWorldUrl = "http://localhost:60634/Article/Details/1";
+        private String proBlogUrl = "http://localhost:60634/Article/Details/2";
+        private String blogUpdateUrl = "http://localhost:60634/Article/Details/3";
+
+        public SitePages(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public void NavigateToHomePage()
+        {
+            this.Driver.Navigate().GoToUrl(this.homeUrl);
+        }
+
+        public void NavigateToRegistrationPage()
+        {
+            this.Driver.Navigate().GoToUrl(this.registrationUrl);
+        }
+
+        public void NavigateToLoginPage()
+        {
+            this.Driver.Navigate().GoToUrl(this.loginUrl);
+        }
+
+        public void NavigateToHelloWorldPage()
+        {
+            this.Driver.Navigate().GoToUrl(this.helloWorldUrl);
+        }
+
+        public void NavigateToProBlogPage()
+        {
+            this.Driver.Navigate().GoToUrl(this.proBlogUrl);
+        }
+
+        public void NavigateToBlogUpdatePage()
+        {
+            this.Driver.Navigate().GoToUrl(this.blogUpdateUrl);
+        }
+    }
+}
